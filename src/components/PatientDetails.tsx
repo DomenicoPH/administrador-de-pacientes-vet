27eab8e6352d51cd10cbc06d01f5c40c747fb7da
Domenico Pagano <dpaganoh@gmail.com>
2026-01-12 12:18:11 -0500

    toast al eliminar paciente

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,6 +1,7 @@
 import type { Patient } from "../types";
 import PatientDetailItem from "./PatientDetailItem";
 import { usePatientStore } from "../store";
+import { toast } from "react-toastify";
 
 type PatientDetailProps = {
   patient: Patient;
@@ -9,6 +10,13 @@ type PatientDetailProps = {
 export default function PatientDetails({ patient }: PatientDetailProps) {
   const deletePatient = usePatientStore((state) => state.deletePatient);
   const getPatientById = usePatientStore((state) => state.getPatientById);
+
+  const handleDelete = () => {
+    deletePatient(patient.id);
+    toast('Paciente eliminado correctamente', {
+        type: 'success'
+    });
+  }
 
   return (
     <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
@@ -29,7 +37,7 @@ export default function PatientDetails({ patient }: PatientDetailProps) {
 
         <button 
             className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
-            onClick={() => deletePatient(patient.id)}
+            onClick={handleDelete}
         >
           Eliminar
         </button>
