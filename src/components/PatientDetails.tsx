f0c398b34d420c70c88ede7c445eedd788e68552
Domenico Pagano <dpaganoh@gmail.com>
2026-01-12 09:20:27 -0500

    renderizando datos de paciente en componente PatientList / Componentes dinámicos: PatientDetails y PatientDetailItem

--- src/components/PatientDetails.tsx
+++ src/components/PatientDetails.tsx
@@ -1,4 +1,5 @@
 import type { Patient } from "../types";
+import PatientDetailItem from "./PatientDetailItem";
 
 type PatientDetailProps = {
     patient: Patient;
@@ -6,8 +7,15 @@ type PatientDetailProps = {
 
 export default function PatientDetails({patient} : PatientDetailProps) {
   return (
-    <div>
-        <h2>{patient.name}</h2>
+    <div className="mx-5 my-10 px-5 py-10 bg-white shadow-md rounded-xl">
+
+        <PatientDetailItem label="ID" data={patient.id} />
+        <PatientDetailItem label="Nombre" data={patient.name} />
+        <PatientDetailItem label="Propietario" data={patient.caretaker} />
+        <PatientDetailItem label="Email" data={patient.email} />
+        <PatientDetailItem label="Fecha" data={patient.date.toString()} />
+        <PatientDetailItem label="Síntomas" data={patient.symptoms} />
+
     </div>
   )
 }
